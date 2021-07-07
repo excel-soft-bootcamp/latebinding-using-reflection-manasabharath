26aafb754b5083d35793f26407b584c3517f27fe
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 15:56:40 +0530

    Add files via upload

---
+++ GameApplication/BasicLevelLib/BasicLevelType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLevelLib
+{
+    public class BasicLevelType
+    {
+       
+        public string Play(string playerName, int creditpoints)
+        {
+            return "Game Started, Level : Basic";
+        }
+    }
+}
