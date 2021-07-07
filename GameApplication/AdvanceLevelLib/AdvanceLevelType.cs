e6353c7f6408eff88981b743cbcf23607353a82b
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 15:57:28 +0530

    Add files via upload

---
+++ GameApplication/AdvanceLevelLib/AdvanceLevelType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceLevelLib
+{
+    public class AdvanceLevelType
+    {
+        public string Begin(string playerName, int creditpoints)
+        {
+            return "Game Started, Level : Advanced";
+        }
+    }
+}
