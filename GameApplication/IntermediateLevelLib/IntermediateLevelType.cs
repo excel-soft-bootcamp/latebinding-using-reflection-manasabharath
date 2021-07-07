e271bc5ac6265f451ec19933a4565faefaabdfeb
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-07 15:57:05 +0530

    Add files via upload

---
+++ GameApplication/IntermediateLevelLib/IntermediateLevelType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntermediateLevelLib
+{
+    public class IntermediateLevelType
+    {
+        public string Start(string playerName, int creditpoints)
+        {
+            return "Game Started, Level : Intermediate";
+        }
+    }
+}
